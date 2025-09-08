d8da22f01c3c10c664d68dd4ffc3cc4d56370867
Sahin00789 <166301251+Sahin00789@users.noreply.github.com>
2025-09-09 04:58:17 +0530

    tonight final updat442

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_URL = 'https://marksmint-dps-paharpur-server.onrender.com/api';
 const API_URL2 = 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL: API_URL2,
+  baseURL: API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',

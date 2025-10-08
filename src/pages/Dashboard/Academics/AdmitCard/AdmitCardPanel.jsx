0f60e1f2b656024da93849a1f750f8a973ebf0c1
Sahin00789 <166301251+Sahin00789@users.noreply.github.com>
2025-10-08 15:40:09 +0530

    admit improvement 2

--- src/pages/Dashboard/Academics/AdmitCard/AdmitCardPanel.jsx
+++ src/pages/Dashboard/Academics/AdmitCard/AdmitCardPanel.jsx
@@ -26,7 +26,7 @@ import { format, parseISO, parse, isBefore } from "date-fns";
 import { enIN } from "date-fns/locale/en-IN";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { debounce } from "lodash";
+import debounce from 'lodash/debounce';
 import AdmitPrintPage from "@/pages/Dashboard/Academics/AdmitCard/AdmitCardPage/AdmitPrintPage";
 import ExamDependentClassSelectorCard from "@/components/common/ExamDependentClassSelectorCard";
 import { getStudentsByClass } from "@/services/students";
